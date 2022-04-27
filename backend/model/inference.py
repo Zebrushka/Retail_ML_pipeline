8682bbbdf5a39f732f5605ceb99a37b760728bd4
Zebrushka <mihail.rsm@yandex.ru>
2022-04-27 10:20:26 +0500

    wrote the backend and the frontend

--- backend/model/inference.py
+++ backend/model/inference.py
@@ -79,4 +79,4 @@ def predict(input_image):
         label = labels_map[idx]
         print('{label:<75} ({p:.2f}%)'.format(label=labels_map[idx], p=probability * 100))
 
-    return label, probability,
+    return label, probability, detections

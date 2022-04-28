f4110bf14eabfecff9ba2bb082743dcde9edf664
Zebrushka <mihail.rsm@yandex.ru>
2022-04-28 15:49:11 +0500

    wrote the backend and the frontend

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,7 @@ import streamlit as st
 from PIL import Image
 import io
 import base64
+import time
 
 # https://discuss.streamlit.io/t/version-0-64-0-deprecation-warning-for-st-file-uploader-decoding/4465
 st.set_option("deprecation.showfileUploaderEncoding", False)
@@ -16,7 +17,7 @@ image = st.file_uploader("Choose an image")
 # displays a button
 if st.button("Get label"):
     if image is not None:
-
+        t = time.time()
         files = {"file": image.getvalue()}
         res = requests.post("http://62.148.235.120:8090/probability", files=files)
 
@@ -25,6 +26,6 @@ if st.button("Get label"):
         probability = resolve.get("probability")
 
         im = Image.open(io.BytesIO(base64.b64decode(resolve.get("result"))))
-
+        st.write("elapsed time : {}s".format(time.time() - t))
         st.write(label, probability)
         st.image(im, width=300)

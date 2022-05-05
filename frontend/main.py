08a189b2f2f734f5d8d28d65a440dceae58abad7
mihail.rsm@yandex.ru <Mihail1995che>
2022-05-05 20:17:30 +0500

    added OCR

--- frontend/main.py
+++ frontend/main.py
@@ -24,19 +24,26 @@ if st.button("Get label"):
         resolve = res.json()
         label = resolve.get("label")
         probability = resolve.get("probability")
+        st.write(label, probability)
         price_res = requests.post("http://62.148.235.120:8090/get_price", files=files)
         price_resolve = price_res.json()
         price = resolve.get("price")
 
         result_image_byts = resolve.get("result")
+
         im = Image.open(io.BytesIO(base64.b64decode(result_image_byts)))
+
+        st.image(im, width=300)
 
         requests.post("http://62.148.235.120:8090/write_db", label, probability, price, result_image_byts)
 
-        st.write("elapsed time : {}s".format(time.time() - t))
-        st.write(label, probability)
         st.write(price)
-        st.image(im, width=300)
+
+
+        st.write("elapsed time : {}s".format(time.time() - t))
+
+
+
 
 
 #TODO сделать раскрывающийся список, при нажатии на него - подгружать историю всех запросов
